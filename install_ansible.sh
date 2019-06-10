42a06c666b9c09817ae197199b3e2cffe6ec2bc2
royselekter <46947390+royselekter@users.noreply.github.com>
2019-06-10 19:20:01 +0300

    Create install_ansible.sh

---
+++ install_ansible.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+apt-add-repository ppa:ansible/ansible -y
+apt-get update -y
+apt-get install ansible -y
+
+
+
+
+ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/install-docker.yml
+
+ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/k8s-common.yml
+ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/k8s-master.yml
+ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/k8s-minion.yml
