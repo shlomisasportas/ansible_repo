304a84462b65e13a128f75e9bf312f71fd5a7546
royselekter <46947390+royselekter@users.noreply.github.com>
2019-06-11 09:50:59 +0300

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
 
-sudo -i
-
-apt-add-repository ppa:ansible/ansible -y
-apt-get update -y
-apt-get install ansible -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update -y
+sudo apt-get install ansible -y
 
 sleep 15
 
 
-ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/install-docker.yml
+sudo ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/install-docker.yml
 
-ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/k8s-common.yml
-ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/k8s-master.yml
-ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/k8s-minion.yml
+sudo ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/k8s-common.yml
+sudo ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/k8s-master.yml
+sudo ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/k8s-minion.yml
