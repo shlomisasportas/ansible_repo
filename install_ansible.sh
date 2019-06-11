4bc6b36b9e31d4fc06ae243bce2079526a405001
royselekter <46947390+royselekter@users.noreply.github.com>
2019-06-11 21:02:40 +0300

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -34,6 +34,8 @@ sleep 10
 
 sudo chown -R ubuntu /home/ubuntu/.ansible
 
+sudo service ssh restart
+
 ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/wait_ssh.yml
 
 ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/install-docker.yml
