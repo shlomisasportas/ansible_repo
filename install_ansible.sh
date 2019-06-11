07deb3556d631b1fa34647a6192541348e11c4d9
royselekter <46947390+royselekter@users.noreply.github.com>
2019-06-11 12:16:41 +0300

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -6,7 +6,11 @@ sudo apt-get install ansible -y
 
 sleep 20
 
-until sudo chown -R ubuntu /home/ubuntu/.ansible; do echo "Retrying"; sleep 5; done
+ssh -o stricthostkeychecking=no ubuntu@192.168.15.51 'sudo echo "UserKnownHostsFile /dev/null" >> ~/.ssh/config'
+ssh -o stricthostkeychecking=no ubuntu@192.168.15.52 'sudo echo "UserKnownHostsFile /dev/null" >> ~/.ssh/config'
+ssh -o stricthostkeychecking=no ubuntu@192.168.10.40 'sudo echo "UserKnownHostsFile /dev/null" >> ~/.ssh/config'
+
+sudo chown -R ubuntu /home/ubuntu/.ansible
 ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/install-docker.yml
 
 ansible-playbook -i /home/ubuntu/ansible_repo/inventory.yml /home/ubuntu/ansible_repo/k8s-common.yml
