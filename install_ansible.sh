47f540df3213d290a332df571d3a9af6c5dc60b8
royselekter <46947390+royselekter@users.noreply.github.com>
2019-06-11 20:40:14 +0300

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,7 +4,7 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -y
 sudo apt-get install ansible -y
 
-sleep 20
+sleep 60
 
 sudo sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
 sudo chown ubuntu /etc/ssh/ssh_config
@@ -12,11 +12,13 @@ sudo echo "UserKnownHostsFile=/dev/null" >> /etc/ssh/ssh_config
 
 sudo service ssh restart
 
-sleep 2 
+sleep 30
 
 ssh -o stricthostkeychecking=no ubuntu@192.168.15.51 'sudo echo "UserKnownHostsFile /dev/null" >> ~/.ssh/config'
 ssh -o stricthostkeychecking=no ubuntu@192.168.15.52 'sudo echo "UserKnownHostsFile /dev/null" >> ~/.ssh/config'
 ssh -o stricthostkeychecking=no ubuntu@192.168.10.40 'sudo echo "UserKnownHostsFile /dev/null" >> ~/.ssh/config'
+
+sleep 5
 
 sudo chown -R ubuntu /home/ubuntu/.ansible
 
